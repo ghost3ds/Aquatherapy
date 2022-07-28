7992ba3657c8a352abebdd48f3f2e2354ccd3c76
Dmitrii Chetyreshnikov <d.chet@bk.ru>
2022-07-28 23:33:58 +0300

    add price and about section

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.reviews__carousel').slick({
+        arrows: true,
+        dots: true,
+        infinite: true,
+        autoplay: true,
+        slidesToShow: 3,
+        slidesToScroll: 1
+    });
+  });
